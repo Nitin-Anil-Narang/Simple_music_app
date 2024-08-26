67437ca2e3bae4604dbf8e42484afcefd18a058a
Nitin Narang aka NN2 <narangnitin520@gmail.com>
2024-08-26 23:13:17 +0530

    finished the project.

--- test.js
+++ test.js
@@ -1,7 +1,19 @@
-function playSound(e){
-    const audios = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    // const btn = document.querySelector(`div[data-key="${e.keyCode}"]`);
-    audios.play()
+function removeTrans(e){
+    if(e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing')   
 }
 
+function playSound(e){ 
+    const audios = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const btn = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    if(!audios) return;
+    btn.classList.add('playing');
+    audios.currentTime = 0;
+    audios.play();
+}
+
+const btns = document.querySelectorAll('.key');
+btns.forEach(btn => btn.addEventListener('transitionend',removeTrans));
 window.addEventListener('keydown',playSound);
+
+
